4321936b88cacc24f45b285c77680942b7ea8f74
Qingyuan Hu <qingyuan.hu@campus.tu-berlin.de>
2024-02-02 00:10:52 +0100

    add cmake build content

---
+++ build/CMakeFiles/webserver.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/huqingyuan/desktop/DHT_network/data.c" "CMakeFiles/webserver.dir/data.c.o" "gcc" "CMakeFiles/webserver.dir/data.c.o.d"
+  "/Users/huqingyuan/desktop/DHT_network/dht.c" "CMakeFiles/webserver.dir/dht.c.o" "gcc" "CMakeFiles/webserver.dir/dht.c.o.d"
+  "/Users/huqingyuan/desktop/DHT_network/http.c" "CMakeFiles/webserver.dir/http.c.o" "gcc" "CMakeFiles/webserver.dir/http.c.o.d"
+  "/Users/huqingyuan/desktop/DHT_network/util.c" "CMakeFiles/webserver.dir/util.c.o" "gcc" "CMakeFiles/webserver.dir/util.c.o.d"
+  "/Users/huqingyuan/desktop/DHT_network/webserver.c" "CMakeFiles/webserver.dir/webserver.c.o" "gcc" "CMakeFiles/webserver.dir/webserver.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
